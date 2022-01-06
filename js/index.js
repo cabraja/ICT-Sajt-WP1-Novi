682a239237117ebccce3ab2394a69c29faa34abb
cabraja <cabrajamihajlo@gmail.com>
2022-01-06 20:52:18 +0100

    wip

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    // Animacija izlaska teksta za svaku osobu pri skrolu
+    $(".card p").hide();
+    $(document).scroll(function(){
+        if(window.scrollY > 160){
+            $(".card p").slideDown(800);
+        }
+    });
+
+})
---
+++ slideShow.js
@@ -0,0 +1,39 @@
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+const translateSlides = () => {
+    slides.forEach(slide => {
+            slide.style.transform = `translateX(-${currentSlide}%)`;
+    })
+}
+
+setInterval(() => {
+
+    currentSlide += 100;
+
+   if(currentSlide < 300){
+       translateSlides();
+   }    
+   else{
+       currentSlide = 0;
+       translateSlides();
+   }
+}, 6000);
+
+
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+leftArrow.addEventListener("click",() => {
+    if(currentSlide > 0){
+        currentSlide -= 100;
+        translateSlides();
+    }
+})
+
+rightArrow.addEventListener("click",() => {
+    if(currentSlide < 200){
+        currentSlide += 100;
+        translateSlides();
+    }
+})
